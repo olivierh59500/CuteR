9591b8f014b776e9a6f2891b8b73239b07a08bfd
Chinuno Usami <cirno@lovelive>
2016-03-12 19:21:54 +0800

    version 1.0

---
+++ CuteR.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+
+from PIL import Image
+from PIL import ImageEnhance
+import qrcode
+
+
+def color_replace(image,color):
+    """Replace black with other color
+
+    :color: custom color (r,g,b)
+    :image: image to replace color
+    :returns: TODO
+
+    """
+    pixels = image.load()
+    size = image.size[0]
+    for width in range(size):
+        for height in range(size):
+            r,g,b = pixels[width,height]
+            if (r,g,b) == (0,0,0):
+                pixels[width,height] = color
+
+
+def produce(txt,img,ver=5,err_crt = qrcode.constants.ERROR_CORRECT_H,bri = 1.0, cont = 1.0,\
+        color = False, rgb = (0,0,0)):
+    """
+
+    :txt: QR text
+    :img: Image
+    :ver: QR version
+    :err_crt: QR error correct
+    :bri: Brightness enhance
+    :cont: Contrast enhance
+    :color: If color mode
+    :rgb: color to replace black
+    :returns: Produced image
+
+    """
+    qr = qrcode.QRCode(version = ver,error_correction = err_crt,box_size=3)
+    qr.add_data(txt)
+    qr.make(fit=True)
+    img_qr = qr.make_image().convert('RGB')
+    if color and ( rgb != (0,0,0) ):
+        color_replace(img_qr,rgb)
+    img_img = Image.open(img).convert('RGBA')
+    if img_img.size[0] < img_img.size[1]:
+        img_img = img_img.resize((img_qr.size[0]-24,int((img_qr.size[1]-24)*(1.0*img_img.size[1]/img_img.size[0]))))
+    else:
+        img_img = img_img.resize(((img_qr.size[0]-24)*(img_img.size[0]/img_img.size[1])),(img_qr.size[1]-24))
+
+    enh = ImageEnhance.Contrast(img_img)
+    img_enh = enh.enhance(cont)
+    enh = ImageEnhance.Brightness(img_enh)
+    img_enh = enh.enhance(bri)
+    if not color:
+        img_enh = img_enh.convert('1')
+        img_qr = img_qr.convert('1')
+    img_res = img_qr
+    for x in range(0,img_enh.size[0]):
+        if x >= 18 and x < 21 :
+            continue
+        for y in range(0,img_enh.size[0]):
+            if y >= 18 and y < 21 or (x%3 ==1 and  y%3 == 1):
+                continue
+            if x < 27 and (y < 27 or y > img_enh.size[0]-25):
+                continue
+            if x > img_enh.size[0]-25 and (y < 27 ):
+                continue
+            if qr.version >6 and x > img_enh.size[0]-34 and (y < 21 ):
+                continue
+            if qr.version >6 and y > img_enh.size[0]-34 and (x < 21 ):
+                continue
+            if img_img.getpixel((x,y))[2] == 0:
+                continue
+            img_res.putpixel((x+12,y+12),img_enh.getpixel((x,y)))
+            # img_res.putpixel((x+12,y+12),img_img.getpixel((x,y)))
+    pos = qrcode.util.pattern_position(qr.version)
+    img_qr2 = qr.make_image().convert("RGB")
+    if color and ( rgb != (0,0,0) ):
+        color_replace(img_qr2,rgb)
+    for i in pos:
+        for j in pos:
+            if (i == 6 and j == pos[-1]) or (j == 6 and i == pos[-1])\
+                or (i == 6 and j == 6):
+                continue
+            else:
+                rect = (3*(i-2)+12,3*(j-2)+12,3*(i+3)+12,3*(j+3)+12)
+                img_tmp = img_qr2.crop(rect)
+                img_res.paste(img_tmp,rect)
+
+    return img_res.resize((img_res.size[0]*10,img_res.size[1]*10))
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Combine your QR code with custom picture")
+    parser.add_argument("image")
+    parser.add_argument("text", help="QRcode Text.")
+    parser.add_argument("-o", "--output", help="Name of output file.")
+    parser.add_argument("-v", "--version", type=int, help="QR version.In range of [1-40]")
+    parser.add_argument("-e", "--errorcorrect", choices={"L","M","Q","H"}, help="Error correct")
+    parser.add_argument("-b", "--brightness", help="Brightness enhance")
+    parser.add_argument("-c", "--contrast", help="Contrast enhance")
+    parser.add_argument("-C", "--color", action="store_true",help="color mode")
+    parser.add_argument("-r", "--rgb", nargs=3, metavar=('R','G','B'),type = int, help="color to replace black")
+    args = parser.parse_args()
+
+    img = args.image
+    txt = args.text
+    output = args.output if args.output else 'qr.png'
+    ec = qrcode.constants.ERROR_CORRECT_H
+    if args.errorcorrect:
+        ec_raw = args.errorcorrect
+        if ec_raw == 'L':
+            ec = qrcode.constants.ERROR_CORRECT_L
+        if ec_raw == 'M':
+            ec = qrcode.constants.ERROR_CORRECT_M
+        if ec_raw == 'Q':
+            ec = qrcode.constants.ERROR_CORRECT_Q
+    ver = 5
+    if args.version:
+        if args.version >= 1 and args.version <= 40:
+            ver = args.version
+    cont = args.contrast if args.contrast else 1.0
+    bri = args.brightness if args.brightness else 1.0
+    colr = True if args.color else False
+    if colr and args.rgb:
+        rgb = tuple(args.rgb)
+    produce(txt,img,ver,ec,bri, cont ,color = colr,rgb=rgb).save(output)
---
+++ README.md
@@ -0,0 +1,66 @@
+# CuteR
+Combine QRCode with picture
+
+Sample command
+```bash
+python CuteR.py sample_input.png http://www.chinuno.com
+python CuteR.py -C -r 0 100 50  sample_input.png http://www.chinuno.com #with color mode
+```
+Sample input
+![image](https://github.com/chinuno-usami/CuteR/raw/master/sample_input.png)
+Sample output
+![image](https://github.com/chinuno-usami/CuteR/raw/master/sample_output.png)
+Sample output with color mode
+![image](https://github.com/chinuno-usami/CuteR/raw/master/sample_output_color.png)
+
+## Usage
+### Import as module
+```python
+Import CuteR as cr
+output = cr.produce(text,image)
+```
+arguments:
+      :txt: QR text
+      :img: Image
+      :ver: QR version
+      :err_crt: QR error correct
+      :bri: Brightness enhance
+      :cont: Contrast enhance
+      :color: If color mode
+      :rgb: color to replace black
+      :returns: Produced image
+
+### As a command tool
+usage: CuteR.py [-h] [-o OUTPUT] [-v VERSION] [-e {Q,H,M,L}] [-b BRIGHTNESS]
+                [-c CONTRAST] [-C] [-r R G B]
+                image text
+
+Combine your QR code with custom picture
+
+positional arguments:
+  image
+  text                  QRcode Text.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Name of output file.
+  -v VERSION, --version VERSION
+                        QR version.In range of [1-40]
+  -e {Q,H,M,L}, --errorcorrect {Q,H,M,L}
+                        Error correct
+  -b BRIGHTNESS, --brightness BRIGHTNESS
+                        Brightness enhance
+  -c CONTRAST, --contrast CONTRAST
+                        Contrast enhance
+  -C, --color           color mode
+  -r R G B, --rgb R G B
+                        color to replace black
+
+## Dependencies
+- Python
+- qrcode
+- PIL or Pillow
+
+## License
+GPLv3
